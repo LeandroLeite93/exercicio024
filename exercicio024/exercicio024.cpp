d554c387a4e2a4bbb18515949059a977677ae999
LeandroLeite93 <leandro93.git@gmail.com>
2023-11-22 17:40:31 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio024/exercicio024.cpp
@@ -0,0 +1,18 @@
+/*
+exercicio024.cpp
+
+escreva um programa que mostre na tela os numeros de 15 ate 20.
+*/ 
+
+#include <iostream>
+
+int main()
+{
+	// int i; - opcional declarar a variavel fora do FOR
+
+	for (int i = 15; i <= 20; i++)
+	{
+		printf_s("%d \n", i);
+	}
+}
+
